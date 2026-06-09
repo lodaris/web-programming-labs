f8b390717879eddf5f53aa6deeb87f89d56db76c
Lodar1s <rostikkodash@gmail.com>
2026-06-09 19:14:30 +0000

    Lab9: implement AuthService - register with bcrypt and login with JWT

---
+++ lab9/task-auth-api/src/auth/dto/login.dto.ts
@@ -0,0 +1,10 @@
+import { IsEmail, IsString, IsNotEmpty } from 'class-validator';
+ 
+export class LoginDto {
+  @IsEmail({}, { message: 'Некоректний формат email' })
+  email: string;
+ 
+  @IsString()
+  @IsNotEmpty({ message: 'Пароль не може бути порожнім' })
+  password: string;
+}
